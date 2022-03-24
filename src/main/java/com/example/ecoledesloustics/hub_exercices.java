0cca88a5174dde585f1f135221d226eaa9b42cf6
blancos <blancosacha38@gmail.com>
2022-03-24 10:01:24 +0100

    project setup

---
+++ src/main/java/com/example/ecoledesloustics/hub_exercices.java
@@ -0,0 +1,14 @@
+package com.example.ecoledesloustics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hub_exercices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hub_exercices);
+    }
+}
